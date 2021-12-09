e944ec2ca22cc9f605c3f0718851f27444a697f6
maria paula alquichire <paula.alquichire27@gmail.com>
2021-12-09 18:02:24 -0500

    ajustes cocina

---
+++ cocina/index.js
@@ -0,0 +1,5 @@
+// import filepond from 'filepond';
+import * as FilePond from 'filepond';
+
+// const inputElement = document.querySelector('input[type="file"]');
+// const pond = FilePond.create(inputElement);
